2a83c6d0920cd67322487c066d08976c095fadd9
kimhyojin <kimhyojin@localhost>
2025-06-28 20:34:51 +0900

    create project

---
+++ alendar/src/main/java/com/example/alendar/AlendarApplication.java
@@ -0,0 +1,13 @@
+package com.example.alendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlendarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlendarApplication.class, args);
+	}
+
+}
---
+++ alendar/src/test/java/com/example/alendar/AlendarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
